d970d399b46722d21aeb41a137b59660f0e0b312
be99inner <anurak.jann@gmail.com>
2022-12-24 13:13:42 +0700

    fix: let telescope load theme from themer

--- lua/configs/motivation/telescope.lua
+++ lua/configs/motivation/telescope.lua
@@ -1,4 +1,5 @@
 local telescope = require("telescope")
 
 telescope.load_extension("themes") -- load extension from themer
+-- telescope.load_extension("lazygit") -- load extension from lazygit
 telescope.setup({})
