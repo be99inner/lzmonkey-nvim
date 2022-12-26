20b827b795073d306b471ca60b6a991b9596a948
be99inner <anurak.jann@gmail.com>
2022-12-27 00:55:23 +0700

    chore: cleanup telescope configs

--- lua/configs/motivation/telescope.lua
+++ lua/configs/motivation/telescope.lua
@@ -1,5 +1,4 @@
 local telescope = require("telescope")
 
 telescope.load_extension("themes") -- load extension from themer
--- telescope.load_extension("lazygit") -- load extension from lazygit
 telescope.setup({})
