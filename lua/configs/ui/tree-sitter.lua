c210cddbe9170b71d2e6b4f31d4999632e5bc83d
be99inner <anurak.jann@gmail.com>
2023-01-02 12:23:49 +0700

    chore: change treesitter to sync install

--- lua/configs/ui/tree-sitter.lua
+++ lua/configs/ui/tree-sitter.lua
@@ -16,7 +16,7 @@ require("nvim-treesitter.configs").setup({
   -- ensure_installed = "all",
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+  sync_install = true,
 
   -- Automatically install missing parsers when entering buffer
   auto_install = true,
