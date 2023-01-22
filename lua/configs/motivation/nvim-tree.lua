9977ebc132f0ef5b790b5d368f2309dfa4635da8
be99inner <anurak.jann@gmail.com>
2023-01-22 13:28:59 +0700

    fix: fix auto close on nvim-tree

--- lua/configs/motivation/nvim-tree.lua
+++ lua/configs/motivation/nvim-tree.lua
@@ -56,16 +56,12 @@ require("nvim-tree").setup({
 })
 
 -- automatically close NvimTree when it's last buffer.
+-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close#naive-solution
 vim.api.nvim_create_autocmd("BufEnter", {
-  group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
-  pattern = "NvimTree_*",
+  nested = true,
   callback = function()
-    local layout = vim.api.nvim_call_function("winlayout", {})
-    if layout[1] == "leaf"
-        and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
-        and layout[3] == nil
-    then
-      vim.cmd("confirm quit")
+    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+      vim.cmd("quit")
     end
   end,
 })
