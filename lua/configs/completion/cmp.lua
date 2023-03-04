78cc8675d94a167bec9e463dbbc97b1587a4f3ba
be99inner <anurak.jann@gmail.com>
2023-03-05 06:46:34 +0700

    chore: enable auto restart

--- lua/configs/completion/cmp.lua
+++ lua/configs/completion/cmp.lua
@@ -12,7 +12,7 @@ require("luasnip.loaders.from_vscode").lazy_load()
 cmp.setup({
   -- preselect
   preselect = cmp.PreselectMode.None,
-  autorestart = false,
+  autorestart = true,
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
@@ -44,7 +44,7 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-b>"] = cmp.mapping.scroll_docs( -4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
@@ -73,16 +73,16 @@ cmp.setup({
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
+      elseif luasnip.jumpable( -1) then
+        luasnip.jump( -1)
       else
         fallback()
       end
     end, { "i", "s" }),
     -- Snippet Jump
     ["<C-j>"] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(-1) then
-        luasnip.jump(-1)
+      if luasnip.jumpable( -1) then
+        luasnip.jump( -1)
       else
         fallback()
       end
